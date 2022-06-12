66e89f85fd54a1484b8227e73515dfa526a3b6a9
blinx <103583498+blinx-co@users.noreply.github.com>
2022-06-12 16:33:14 +0100

    Update ClickingMadness.lua

--- Scripts/ClickingMadness.lua
+++ Scripts/ClickingMadness.lua
@@ -3,63 +3,66 @@ local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/blinx-co/
 local win = lib.CreateWindow("ClickingMadness", Color3.fromRGB(25,65,188))
 local p = game:GetService("ReplicatedStorage").Aero.AeroRemoteServices
 
-getgenv().autofarm = false;
-getgenv().autorebirth = false;
-getgenv().buyEgg = false;
-
+getgenv().tap = false
+getgenv().rebirth = false
+getgenv().egg = false
 
 function doTap()
     spawn(function()
-        while getgenv().autofarm == true do 
-            local a = {[1]=1}
+        while getgenv().tap == true do
+            local a = {
+                [1] = 1
+            }
             p.ClickService.Click:FireServer(unpack(a))
             wait()
-        end    
-    end)    
+        end
+    end)
 end
 
-function doRebirth(amount) 
+function doRebirth(amount)
     spawn(function()
-        while autoRebirth == true do
+        while rebirth == true do
             local args = {
                 [1] = amount
             }
             p.RebirthService.BuyRebirths:FireServer(unpack(args))
             wait()
         end
-    end) 
+    end)
 end
 
-function doEgg(type)
+function doEgg(eType)
     spawn(function()
-    while wait() do
-        if not buyEgg then
-            break
+        while wait() do
+            if not egg then break end
+            p.EggService.Purchase:FireServer(eType)
         end
-        ars.EggService.Purchase:FireServer(type)
-    end
-end)
+    end)
+end
 
 win.CreateButton("Destroy GUI", function()
-    win.DestroyGui()    
+    win.DestroyGui()
 end)
 
 win.CreateToggle("Auto Tap", function(bool)
-    getgenv().autofarm = bool
-    if bool then 
-        doTap()    
+    getgenv().tap = bool
+    if bool then
+        doTap()
     end
 end)
 
 win.CreateToggle("Auto Rebirth", function(bool)
-    getgenv().autofarm = bool
-    if bool then 
-        doRebirth(1000)   
+    getgenv().rebirth = bool
+    if bool then
+        doRebirth()
     end
 end)
 
-win.CreateButton("Rage", function()
-    game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.BeastModeService.Begin:FireServer()
+win.CreateToggle("Auto Hatch", function(bool)
+    getgenv().egg = bool
+    if bool then 
+        doEgg('basic')
+    end
 end)
 
 function teleportTo(a)
@@ -69,18 +72,15 @@ function teleportTo(a)
     end
 end
 
-function teleportToWorld(args)
+function teleportToWorld(worldName)
     if game:GetService("Workspace").Worlds:FindFirstChild(worldName) then
         teleportTo(game:GetService("Workspace").Worlds[worldName].Teleport.CFrame)
     end
 end
-
-local selectedWorld = nil
 
 win.CreateDropdown("Worlds",
     {"Lava", "Desert", "Ocean", "Winter", "Toxic", "Candy", "Forst", "Storm", "Blocks", "Space", "Dominus", "Infinity",
      "Future", "City", "Moon", "Fire"}, function(value)
         local plr = game.Players.LocalPlayer
         plr.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Worlds[value].Teleport.CFrame
-     end)
-end
+end)
